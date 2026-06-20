331ee4bebe10caa92381c49f6c51e862cd7ac25a
Uday <udaypolishetty54@gmail.com>
2026-06-20 11:11:39 +0530

    Update settings.py

--- RajuMobile_Backend/backend/backend/settings.py
+++ RajuMobile_Backend/backend/backend/settings.py
@@ -71,6 +71,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:3000",
     "http://127.0.0.1:5173",
+    "https://mobile-project-8.vercel.app",
 ]
 CORS_ALLOW_CREDENTIALS = True
 
