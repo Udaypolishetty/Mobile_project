796323f6a15af4bba12a8ff324695f7431c5a6f8
MaruthiVemula <maruthivemula95@gmail.com>
2026-06-20 22:26:09 +0530

    Update settings.py

--- RajuMobile_Backend/backend/backend/settings.py
+++ RajuMobile_Backend/backend/backend/settings.py
@@ -77,7 +77,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:3000",
     "http://127.0.0.1:5173",
-    "https://mobile-project-8.vercel.app",
+    "https://raju-mobiles.vercel.app",
 ]
 CORS_ALLOW_CREDENTIALS = True
 
