d70c83ddf2d37688862099df1a9179d10af5b763
Uday <udaypolishetty54@gmail.com>
2026-06-20 11:17:41 +0530

    Update ProducttApi.js

--- RajuMobile_Frontend/src/api/ProducttApi.js
+++ RajuMobile_Frontend/src/api/ProducttApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const PRODUCT_URL = "VITE_API_URL/api/products/";
+const PRODUCT_URL = '${import.meta.env.VITE_API_URL}/api/products/';
 
 export const getProducts = async () => {
   const response = await axios.get(PRODUCT_URL);
