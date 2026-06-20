e60f0a77fc0fab6a3df40d0da27b2129006bde2d
Uday <udaypolishetty54@gmail.com>
2026-06-20 11:20:29 +0530

    Update productApi.js

--- RajuMobile_Frontend/src/api/productApi.js
+++ RajuMobile_Frontend/src/api/productApi.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "VITE_API_URL/api/products/",
+  baseURL: `${import.meta.env.VITE_API_URL}/api/products/`,
 });
 
 export const getProducts = () => API.get("/");
